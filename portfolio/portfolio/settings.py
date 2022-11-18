75093e5ddc4960f0d672480834199d79f26895f5
achyut kadam <achyut.k@acldigital.com>
2022-11-18 14:22:27 +0530

    install environ

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -10,6 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import environ
+# Reading env variables
+env = environ.Env()
+environ.Env.read_env()
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env.str('SECRET_KEY')
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = env.bool('DEBUG')
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
